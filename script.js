3beb47f21ff8c103acd661bdfe9afb2879c2da22
Mostafa Fawzy <mostafafawzy7107@gmail.com>
2020-07-07 22:25:05 +0200

    Implement Sign Up Form Validation

--- script.js
+++ script.js
@@ -2,6 +2,51 @@ const API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
 const API_PATH_SIGNUP = "/auth/register";
 const API_PATH_SIGNIN = "/auth/login";
 
+// Implementation Of Checking Validation
+
+function checkValidity(formData) {
+  const fullName = formData.get("name");
+  const email = formData.get("email");
+  const userName = formData.get("username");
+  const password = formData.get("password");
+
+  if (!fullName) {
+    document.querySelector(".errorName").style.display = "block";
+    document.getElementById("name").classList.add("error");
+  }
+
+  if (!email) {
+    document.querySelector(".errorEmail").style.display = "block";
+    document.getElementById("email").classList.add("error");
+  }
+
+  if (!userName) {
+    document.querySelector(".errorUser").style.display = "block";
+    document.getElementById("username").classList.add("error");
+  }
+
+  if (!password) {
+    document.querySelector(".errorPass").style.display = "block";
+    document.getElementById("password").classList.add("error");
+  }
+
+  const allInvalidElems = document
+    .getElementById("signUpForm")
+    .querySelectorAll(".error");
+  if (allInvalidElems.length) {
+    allInvalidElems.forEach((elem) => {
+      elem.addEventListener("input", function () {
+        this.classList.remove("error");
+        this.nextElementSibling.style.display = "none";
+      });
+    });
+
+    return;
+  }
+
+  return true;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Register/Login Switching Functionality (Desktop)
 
@@ -39,12 +84,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const signInForm = document.getElementById("signInForm");
   const logInVerify = document.getElementById("logInVerify");
   const logInButton = document.querySelector(".signInButton");
-  const logInSpinner = document.querySelector('.logInSpinner');
+  const logInSpinner = document.querySelector(".logInSpinner");
 
   signInForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    logInSpinner.style.display = 'block';
+    logInSpinner.style.display = "block";
 
     const formData = new FormData(signInForm);
 
@@ -60,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       })
       .then((data) => {
-        logInSpinner.style.display = 'none';
+        logInSpinner.style.display = "none";
         logInVerify.innerText = data.message;
         logInButton.style.cursor = "not-allowed";
         logInButton.disabled = true;
@@ -73,14 +118,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const signUpForm = document.getElementById("signUpForm");
   const signUpVerify = document.getElementById("signUpVerify");
   const registerButton = document.querySelector(".signUpButton");
-  const signUpSpinner = document.querySelector('.signUpSpinner');
+  const signUpSpinner = document.querySelector(".signUpSpinner");
 
   signUpForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    signUpSpinner.style.display = 'block';
-
     const formData = new FormData(signUpForm);
+
+    const isRegisterValid = checkValidity(formData);
+
+    if (!isRegisterValid) return;
+
+    signUpSpinner.style.display = "block";
 
     fetch(`${API_URL}${API_PATH_SIGNUP}`, {
       method: "POST",
@@ -94,7 +143,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       })
       .then((data) => {
-        signUpSpinner.style.display = 'none';
+        signUpSpinner.style.display = "none";
         signUpVerify.innerText = data.message;
         registerButton.style.cursor = "not-allowed";
         registerButton.disabled = true;
