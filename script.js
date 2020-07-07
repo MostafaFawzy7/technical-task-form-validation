7dcf73063cad6b0e7e0fbe6628cc1b3eb76f682e
Mostafa Fawzy <mostafafawzy7107@gmail.com>
2020-07-07 08:59:26 +0200

    Add DOM Content Load Event

--- script.js
+++ script.js
@@ -2,35 +2,36 @@ const API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
 const API_PATH_SIGNUP = "/auth/register";
 const API_PATH_SIGNIN = "/auth/login";
 
+document.addEventListener("DOMContentLoaded", function () {
+  
+  // Register/Login Switching Functionality (Desktop)
 
-// Register/Login Switching Functionality (Desktop)
+  const signUpButton = document.getElementById("signUp");
+  const signInButton = document.getElementById("signIn");
+  const container = document.getElementById("container");
 
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
+  signUpButton.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+  });
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
+  signInButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+  });
 
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+  // Register/Login Switching Functionality (Mobile)
 
+  const mobLogIn = document.querySelector(".mobLogIn");
+  const mobRegister = document.querySelector(".mobRegister");
+  const signInContainer = document.querySelector(".sign-in-container");
+  const signUpContainer = document.querySelector(".sign-up-container");
 
-// Register/Login Switching Functionality (Mobile)
+  mobLogIn.addEventListener("click", () => {
+    signUpContainer.style.display = "none";
+    signInContainer.style.display = "block";
+  });
 
-const mobLogIn = document.querySelector(".mobLogIn");
-const mobRegister = document.querySelector(".mobRegister");
-const signInContainer = document.querySelector(".sign-in-container");
-const signUpContainer = document.querySelector(".sign-up-container");
-
-mobLogIn.addEventListener("click", () => {
-  signUpContainer.style.display = "none";
-  signInContainer.style.display = "block";
-});
-
-mobRegister.addEventListener("click", () => {
-  signInContainer.style.display = "none";
-  signUpContainer.style.display = "block";
+  mobRegister.addEventListener("click", () => {
+    signInContainer.style.display = "none";
+    signUpContainer.style.display = "block";
+  });
 });
