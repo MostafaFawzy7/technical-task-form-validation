80b898107a93978bc1540315bc2a27b996185697
Mostafa Fawzy <mostafafawzy7107@gmail.com>
2020-07-07 03:08:29 +0200

    Implement Switch Function on Responsive

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-const API_URL = 'https://private-b2e6827-robustatask.apiary-mock.com';
-const API_PATH_SIGNUP = '/auth/register';
-const API_PATH_SIGNIN = '/auth/login';
+const API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
+const API_PATH_SIGNUP = "/auth/register";
+const API_PATH_SIGNIN = "/auth/login";
 
 
 // Register/Login Switching Functionality (Desktop)
 
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
 
 
+// Register/Login Switching Functionality (Mobile)
+
+const mobLogIn = document.querySelector(".mobLogIn");
+const mobRegister = document.querySelector(".mobRegister");
+const signInContainer = document.querySelector(".sign-in-container");
+const signUpContainer = document.querySelector(".sign-up-container");
+
+mobLogIn.addEventListener("click", () => {
+  signUpContainer.style.display = "none";
+  signInContainer.style.display = "block";
+});
+
+mobRegister.addEventListener("click", () => {
+  signInContainer.style.display = "none";
+  signUpContainer.style.display = "block";
+});
