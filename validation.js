bf087b00d6af390d8abbc740a8aae2714336e2ae
Mostafa Fawzy <mostafafawzy7107@gmail.com>
2020-07-09 01:47:35 +0200

    Refactor Validation File

--- validation.js
+++ validation.js
@@ -1,85 +1,83 @@
+// Check Register Form Valdiation
 
-// Implementation Of Checking Register Form Validation
+// Regex patterns
 
-function checkValidity(formData) {
-	const fullName = formData.get('name');
-	const email = formData.get('email');
-	const userName = formData.get('username');
-	const password = formData.get('password');
+const emailPattern = /^([a-zA-Z0-9.]{1,64})+\@([a-zA-Z0-9.]{1,64})+$/;
+const userNamePattern = /[^A-Za-z0-9]+/g;
 
-    // Validate Full Name
-	if(!fullName) {
-		document.querySelector('.errorName').style.display = 'block';
-		document.getElementById('name').classList.add('error');
-	}
+// Validation Of Each Field
 
-    // Validate Email
-	const emailPattern = /^([a-zA-Z0-9.]{1,64})+\@([a-zA-Z0-9.]{1,64})+$/;
-    
-    if(!email || !emailPattern.test(email)) {
-		document.querySelector('.errorEmail').style.display = 'block';
-		document.getElementById('email').classList.add('error');
-	}
+const validateEachField = (allInvalidElems) => {
+  allInvalidElems.forEach((elem) => {
+    elem.addEventListener("input", function () {
+      this.classList.remove("error");
+      this.nextElementSibling.style.display = "none";
+    });
+  });
+};
 
-    // Validate User Name
-	const userNamePattern = /[^A-Za-z0-9]+/g;
-    
-    if(!userName || userNamePattern.test(userName)) {
-		document.querySelector('.errorUser').style.display = 'block';
-		document.getElementById('username').classList.add('error');
-	}
+// Check Regsister Form Valdiation
 
-    // Validate Password
-	if(!password || password.length < 8) {
-		document.querySelector('.errorPass').style.display = 'block';
-		document.getElementById('password').classList.add('error');
-	}
+const checkSignUpValidity = (formData) => {
+  const fullName = formData.get("name");
+  const email = formData.get("email");
+  const userName = formData.get("username");
+  const password = formData.get("password");
 
-	const allInvalidElems = document.getElementById('signUpForm').querySelectorAll('.error');
-    
-    if (allInvalidElems.length) {
-		allInvalidElems.forEach(elem => {
-			elem.addEventListener('input', function () {
-				this.classList.remove('error');
-				this.nextElementSibling.style.display = 'none';
-			})
-		});
+  if (!fullName) {
+    document.querySelector(".errorName").style.display = "block";
+    document.getElementById("name").classList.add("error");
+  }
 
-		return;
-	}
+  if (!email || !emailPattern.test(email)) {
+    document.querySelector(".errorEmail").style.display = "block";
+    document.getElementById("email").classList.add("error");
+  }
 
-	return true;
-}
+  if (!userName || userNamePattern.test(userName)) {
+    document.querySelector(".errorUser").style.display = "block";
+    document.getElementById("username").classList.add("error");
+  }
 
+  if (!password || password.length < 8) {
+    document.querySelector(".errorPass").style.display = "block";
+    document.getElementById("password").classList.add("error");
+  }
 
-// Implementation Of Checking Login Form Validation
+  const allInvalidElems = document.getElementById("signUpForm").querySelectorAll(".error");
 
-function checkSignInValidity(formData) {
-	const logInEmail = formData.get('login');
-    const logInPass = formData.get('pass');;
+  if (allInvalidElems.length) {
+    validateEachField(allInvalidElems);
 
-    const emailPattern = /^([a-zA-Z0-9.]{1,64})+\@([a-zA-Z0-9.]{1,64})+$/;
-	if(!logInEmail || !emailPattern.test(logInEmail)) {
-		document.querySelector('.errEmail').style.display = 'block';
-		document.getElementById('logInEmail').classList.add('error');
-	}
+    return;
+  }
 
-	if(!logInPass || logInPass.length < 8) {
-		document.querySelector('.errPass').style.display = 'block';
-		document.getElementById('logInPass').classList.add('error');
-	}
+  return true;
+};
 
-	const allInvalidElems = document.getElementById('signInForm').querySelectorAll('.error');
-	if (allInvalidElems.length) {
-		allInvalidElems.forEach(elem => {
-			elem.addEventListener('input', function () {
-				this.classList.remove('error');
-				this.nextElementSibling.style.display = 'none';
-			})
-		});
+// Check Login Form Valdiation
 
-		return;
-	}
+const checkSignInValidity = (formData) => {
+  const logInEmail = formData.get("login");
+  const logInPass = formData.get("pass");
 
-	return true;
-}
+  if (!logInEmail || !emailPattern.test(logInEmail)) {
+    document.querySelector(".errEmail").style.display = "block";
+    document.getElementById("logInEmail").classList.add("error");
+  }
+
+  if (!logInPass || logInPass.length < 8) {
+    document.querySelector(".errPass").style.display = "block";
+    document.getElementById("logInPass").classList.add("error");
+  }
+
+  const allInvalidElems = document.getElementById("signInForm").querySelectorAll(".error");
+
+  if (allInvalidElems.length) {
+    validateEachField(allInvalidElems);
+
+    return;
+  }
+
+  return true;
+};
