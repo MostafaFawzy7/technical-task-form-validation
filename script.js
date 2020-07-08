6b6121f06e0a6aeadec0b0a615eccdcbfec8dc57
Mostafa Fawzy <mostafafawzy7107@gmail.com>
2020-07-09 01:55:56 +0200

    Erase Repetitive Variables

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
-const API_PATH_SIGNUP = "/auth/register";
-const API_PATH_SIGNIN = "/auth/login";
-
 
 document.addEventListener("DOMContentLoaded", function () {
   
